75f57ff7f6ba7350bde3cd670929ee4a65abac2a
Zyy993 <slmnalfrsy4707@gmail.com>
2024-05-30 14:55:16 +0700

    first commit

---
+++ src/components/Artikel.vue
@@ -0,0 +1,9 @@
+
+<template>
+  <div>
+  <img src= "../assets/pak-kadis.png" alt="" width="100px">
+    <h1> Ini adalah Artikel</h1>
+    <p>Ini adalah isi</p>
+    
+  </div>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Artikel from '../components/Artikel.vue'
+</script>
+
+<template>
+  <main>
+    <Artikel />
+  </main>
+</template>
